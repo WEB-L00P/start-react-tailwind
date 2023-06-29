237f04cef046ab2f2e20b03ff04900689f527050
WEB-L00P <105985281+WEB-L00P@users.noreply.github.com>
2023-06-29 13:20:57 +0530

    Create Home.jsx

---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+
+const Home = () => {
+  return (
+    <>
+      <div className="mt-10">
+        <h1 className="text-3xl font-bold text-center text-[#525FE1]" >Welcome</h1>
+        <p className="text-center mt-5">Your project is running!</p>
+
+        <p className="text-center mt-5">
+          You can start editing Home.jsx file
+        </p>
+
+        <p className="flex font-semibold justify-center mt-5 text-[#FFA41B]">
+          -Project <br />
+          |- src <br />
+          |-- Home.jsx<br />
+        </p>
+        <p className="text-center font-bold mt-10 text-[#0E2954]">Happy Hacking!</p>
+        <p className="text-center font-bold mt-10 text-[#F86F03]">Developed by Omkar</p>
+      </div >
+    </>
+  )
+}
+
+export default Home
